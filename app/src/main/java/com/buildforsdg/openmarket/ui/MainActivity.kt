d1075a8771ca0804a09f8856ad75c358e8c683f3
clevmania <ernestcleverest@yahoo.com>
2020-08-31 09:57:39 +0100

    refactor: setup packages

--- app/src/main/java/com/buildforsdg/openmarket/ui/MainActivity.kt
+++ app/src/main/java/com/buildforsdg/openmarket/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.buildforsdg.openmarket
+package com.buildforsdg.openmarket.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.buildforsdg.openmarket.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
