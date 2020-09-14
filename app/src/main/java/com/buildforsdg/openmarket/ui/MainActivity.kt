a40e4aa74d703bb7e79dc9d362db613e4bfdadf8
clevmania <ernestcleverest@yahoo.com>
2020-09-14 15:44:22 +0100

    feat: setup navigation graph

--- app/src/main/java/com/buildforsdg/openmarket/ui/MainActivity.kt
+++ app/src/main/java/com/buildforsdg/openmarket/ui/MainActivity.kt
@@ -2,11 +2,32 @@ package com.buildforsdg.openmarket.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.buildforsdg.openmarket.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val navController by lazy { findNavController(R.id.nav_host_fragment)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+
+            ), null
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
