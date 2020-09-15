8a4ce26006c719662d124244c69792c530921111
clevmania <ernestcleverest@yahoo.com>
2020-09-15 20:58:31 +0100

    feat: created the login service

---
+++ app/src/main/java/com/buildforsdg/openmarket/ui/auth/model/GoogleAuthRequest.kt
@@ -0,0 +1,7 @@
+package com.buildforsdg.openmarket.ui.auth.model
+
+/**
+ * @author by Lawrence on 9/15/20.
+ * for OpenMarket
+ */
+data class GoogleAuthRequest(val idToken: String)
---
+++ app/src/main/java/com/buildforsdg/openmarket/ui/auth/model/LoginRequest.kt
@@ -0,0 +1,11 @@
+package com.buildforsdg.openmarket.ui.auth.model
+
+/**
+ * @author by Lawrence on 9/15/20.
+ * for OpenMarket
+ */
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
